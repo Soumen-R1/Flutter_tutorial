8a638291dee0192e00d5b43154fbf55309aaf83f
Soumen <git15hub12@gmail.com>
2023-02-22 17:23:08 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_2/home_page.dart';
+import 'package:flutter_2/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: homepage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/homepage",
+      routes: {
+        "/": (context) => loginpage(),
+        "/homepage": (context) => homepage(),
+        "/Login": (context) => loginpage()
+      },
+    );
   }
 }
